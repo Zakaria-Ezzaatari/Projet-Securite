e5f8ea688ae40e0787200ea88c1e41ac5a0b308f
Zakaria-Ezzaatari <zakaria.ezzaatari@laposte.net>
2021-10-16 23:36:01 +0200

    Connexion Serveur

--- bdd.php
+++ bdd.php
@@ -1,9 +1,17 @@
 <?php
 	session_start();
-	$db_host = "localhost";
-	$db_name = "tp_securite";
-	$db_user = "root";
-	$db_pass = "";
+	//Connexion Locale
+	//$db_host = "localhost";
+	//$db_name = "tp_securite";
+	//$db_user = "root";
+	//$db_pass = "";
+
+	//Connexion Serveur
+	$db_host = "remotemysql.com";
+	$db_name = "x6UZTFUtZ6";
+	$db_user = "x6UZTFUtZ6";
+	$db_pass = "XQ6oaYCgmz";
+	
 	$bdd=mysqli_connect("$db_host","$db_user","$db_pass","$db_name");
 	if (mysqli_connect_errno())
 	{
